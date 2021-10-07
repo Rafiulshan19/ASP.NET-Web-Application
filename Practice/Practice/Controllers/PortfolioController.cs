55e8a91ba0344009fd705a948896e4f233a8ddab
Rafiulshan19 <18-38321-2@student.aiub.edu>
2021-10-08 01:07:26 +0600

    Portfolio

---
+++ Practice/Practice/Controllers/PortfolioController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Bio()
+        {
+            //ViewBag.Message = "I am an AIUBian. I want to become Millionior. Honesty is the best policy but being honest you can't become millioniar.";
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            string[] names = new string[2];
+                names[0] = "UMS";
+                names[1] = "AMS";
+
+            ViewBag.Names = names;
+
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice/Practice/Controllers/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practice.Controllers
+{
+    internal class Student
+    {
+        public string SName { get; internal set; }
+        public int SId { get; internal set; }
+        public string SAddress { get; internal set; }
+
+        public static implicit operator Student(Student v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
