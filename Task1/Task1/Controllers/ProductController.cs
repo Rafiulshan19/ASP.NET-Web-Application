a8a0f12560b84320802a35ff34cc7744800c9f46
Rafiulshan19 <18-38321-2@student.aiub.edu>
2021-10-10 02:09:47 +0600

    CRUD op

---
+++ Task1/Task1/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task1.Models;
+using Task1.Models.Entity;
+using Task1.Models.Tables;
+using System.Data.SqlClient;
+
+namespace Task1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            Database db = new Database();
+            var products = db.Products.GetAll();
+            return View(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Products.Add(p);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Task1/Task1/Models/Tables/Products.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Task1.Models.Entity;
+
+namespace Task1.Models.Tables
+{
+    public class Products
+    {
+        private SqlConnection connection;
+
+        public Products(SqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void Add(Product p)
+        {
+            string query = string.Format("Insert into Products values ('{0}','{1}','{2}')", p.ProductId, p.ProductName, p.Price);
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int r = command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public Product Get(int id)
+        {
+            return null;
+        }
+        public List<Product> GetAll()
+        {
+            string query = "select * from Products";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Product> students = new List<Product>();
+            while (reader.Read())
+            {
+                Product p = new Product()
+                {
+                    ProductId = reader.GetInt32(reader.GetOrdinal("ProductId")),
+                    ProductName = reader.GetString(reader.GetOrdinal("ProductName")),
+                    Price = reader.GetInt32(reader.GetOrdinal("Price")),
+                    
+                };
+                
+                products.Add(p);
+            }
+            connection.Close();
+            return products;
+        }
+    }
+}
