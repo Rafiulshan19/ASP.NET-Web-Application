f88580b5731b52bba75adfd5969478c1415dbdd9
Rafiulshan19 <18-38321-2@student.aiub.edu>
2021-10-14 01:30:19 +0600

    task

---
+++ ExtendTask/Task1/Task1/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task1.Models;
+using Task1.Models.Entity;
+using Task1.Models.Tables;
+using System.Data.SqlClient;
+
+namespace Task1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private object products;
+        // GET: Product
+        public ActionResult Index()
+        {
+            Database db = new Database();
+            var products = db.Products.GetAll();
+            return View(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Products.Add(p);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        //
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            Database db = new Database();
+            var p = db.Products.GetId(id);
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Product p, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Products.Update(p, id);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Database db = new Database();
+            db.Products.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ViewDetails(int id)
+        {
+            Database db = new Database();
+            var p = db.Products.GetId(id);
+            return View(p);
+        }
+
+    }
+}
---
+++ ExtendTask/Task1/Task1/Models/Tables/Products.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Task1.Models.Entity;
+
+namespace Task1.Models.Tables
+{
+    public class Products
+    {
+        SqlConnection connection;
+
+        public Products(SqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void Add(Product p)
+        {
+            string query = string.Format("Insert into Products values ('{0}','{1}','{2}','{3}')", p.ProductName, p.Price,p.Quantity,p.Description);
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public Product GetId(int id)
+        {
+            string query = "select *  from Products where ProductId =" + id;
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            Product p = new Product();
+            if (reader.Read())
+            {
+
+                p.ProductId = reader.GetInt32(reader.GetOrdinal("id"));
+                p.ProductName = reader.GetString(reader.GetOrdinal("ProductName"));
+                p.Price = reader.GetInt32(reader.GetOrdinal("Price"));
+                p.Quantity = reader.GetInt32(reader.GetOrdinal("Quantity"));
+                p.Description = reader.GetString(reader.GetOrdinal("Description"));
+
+            }
+            connection.Close();
+            return p;
+        }
+        public List<Product> GetAll()
+        {
+            string query = "select * from Products";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Product> students = new List<Product>();
+            while (reader.Read())
+            {
+                Product p = new Product()
+                {
+                    ProductId = reader.GetInt32(reader.GetOrdinal("ProductId")),
+                    ProductName = reader.GetString(reader.GetOrdinal("ProductName")),
+                    Price = reader.GetInt32(reader.GetOrdinal("Price")),
+                    Quantity = reader.GetInt32(reader.GetOrdinal("Quantity")),
+                    Description = reader.GetString(reader.GetOrdinal("Description"))
+
+                };
+                
+                products.Add(p);
+            }
+            connection.Close();
+            return products;
+        }
+
+        public void Delete(int id)
+        {
+            string query = "Delete from Products where ProductId=" + id;
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Update(Product p, int id)
+        {
+            string query = "update Products set ProductName='" + p.ProductName + "', Price='" + p.Price + "',Quantity='" + p.Quantity + "' where ProductId=" + id;
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
